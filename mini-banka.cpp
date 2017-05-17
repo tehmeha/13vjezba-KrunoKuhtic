61580d8f0d4e16f31519f28816647d10e8c98839
KrunoKuhtic <krunokuhtic2@gmail.com>
2017-05-17 12:09:05 +0200

    Ispravak upisa osobe

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -18,13 +18,19 @@ int main()
     int brojKlijenta = 0;
     if ( izbor == 1 )
     {
-        cout<< "Unesite broj racuna"
-        cin>>brRacuna[brojKlijenta]
-        cout<< "Unesite prezime i ime"
+        cout<< "Unesite broj racuna";
+        cin>>brRacuna[brojKlijenta];
+        cout<< "Unesite prezime i ime";
+        cin.ignore();
         getline(cin, prezimeIme[brojKlijenta]);
         saldo[brojKlijenta] = 0;
         brojKlijenta++;
-
-
+    }
+    else if (izbor == 2 )
+    {
+        for( int i = 0; i < brojKlijenta; i++)
+        {
+            cout << brRacuna[i] << " " <<prezimeIme[i] << "saldo: " << saldo[i] <<endl;
+        }
     }
 }

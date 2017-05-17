b2a6663088890db2e900fc9fe06d7beee657fcb8
Filip184 <filip.regovic@gmail.com>
2017-05-17 11:53:16 +0200

    Korisnik unosi novu osobu

--- mini-banka.cpp
+++ mini-banka.cpp
@@ -11,4 +11,20 @@ int main()
     cout << "5. Ispisi sortirano" << endl;
     int izbor;
     cin >> izbor;
+
+    unsigned long long int brRacuna[50];
+    string prezimeIme[50];
+    float saldo[50];
+    int brojKlijenta = 0;
+    if ( izbor == 1 )
+    {
+        cout<< "Unesite broj racuna"
+        cin>>brRacuna[brojKlijenta]
+        cout<< "Unesite prezime i ime"
+        getline(cin, prezimeIme[brojKlijenta]);
+        saldo[brojKlijenta] = 0;
+        brojKlijenta++;
+
+
+    }
 }
